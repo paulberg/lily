8592fb10d57611c9b29220a3f93569d8ee2ccb2f
Paul Berg <paul@paulwberg.com>
2024-05-12 00:14:24 -0700

    Fixing permissions

--- deploy-stackset.sh
+++ deploy-stackset.sh
@@ -9,7 +9,6 @@ AWS_REGION="us-west-2"
 TEMPLATE_FILE="stackset.yml"
 STACKSET_NAME="LilyHelloWorldStackSet"
 TARGET_ACCOUNT_IDS=""
-ROLE_NAME="LilyStackSetExecutionRole"
 
 # Parse command-line parameters
 while [[ $# -gt 0 ]]; do
@@ -47,19 +46,20 @@ if [ -z "$TARGET_ACCOUNT_IDS" ]; then
   TARGET_ACCOUNT_IDS=$(aws sts get-caller-identity --query 'Account' --output text)
 fi
 
-# Check if the role already exists
-aws iam get-role --role-name $ROLE_NAME > /dev/null 2>&1 || {
-  echo "Creating role $ROLE_NAME..."
 
-    # Define the trust policy for the role
-  TRUST_POLICY=$(cat <<EOF
+# Check if the AWSCloudFormationStackSetAdministrationRole exists
+aws iam get-role --role-name AWSCloudFormationStackSetAdministrationRole > /dev/null 2>&1 || {
+    echo "Creating role AWSCloudFormationStackSetAdministrationRole..."
+
+  # Define the trust policy for the AWSCloudFormationStackSetAdministrationRole
+  ADMIN_TRUST_POLICY=$(cat <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
     {
       "Effect": "Allow",
       "Principal": {
-          "Service": "cloudformation.amazonaws.com"
+        "Service": "cloudformation.amazonaws.com"
       },
       "Action": "sts:AssumeRole"
     }
@@ -68,34 +68,62 @@ aws iam get-role --role-name $ROLE_NAME > /dev/null 2>&1 || {
 EOF
 )
 
-  # Create the role
-  aws iam create-role --role-name $ROLE_NAME --assume-role-policy-document "$TRUST_POLICY"
+  # Create the AWSCloudFormationStackSetAdministrationRole
+  aws iam create-role --role-name AWSCloudFormationStackSetAdministrationRole --assume-role-policy-document "$ADMIN_TRUST_POLICY"
 }
 
-# Attach or update the permissions policy on the role
-aws iam put-role-policy --role-name $ROLE_NAME --policy-name StackSetExecutionPolicy --policy-document file://stackset-execution-policy.json
+# Check if the AWSCloudFormationStackSetExecutionRole exists
+aws iam get-role --role-name AWSCloudFormationStackSetExecutionRole > /dev/null 2>&1 || {
+    echo "Creating role AWSCloudFormationStackSetExecutionRole..."
+
+  # Define the trust policy for the AWSCloudFormationStackSetExecutionRole
+  EXECUTION_TRUST_POLICY=$(cat <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::${TARGET_ACCOUNT_IDS}:role/AWSCloudFormationStackSetAdministrationRole"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+)
+
+  # Create the AWSCloudFormationStackSetExecutionRole
+  aws iam create-role --role-name AWSCloudFormationStackSetExecutionRole --assume-role-policy-document "$EXECUTION_TRUST_POLICY"
+}
+
+# Attach the permissions policy to the AWSCloudFormationStackSetAdministrationRole
+aws iam put-role-policy --role-name AWSCloudFormationStackSetAdministrationRole --policy-name StackSetAdministrationPolicy --policy-document file://stackset-administration-policy.json
+
+# Attach the permissions policy to the AWSCloudFormationStackSetExecutionRole
+aws iam put-role-policy --role-name AWSCloudFormationStackSetExecutionRole --policy-name StackSetExecutionPolicy --policy-document file://stackset-execution-policy.json
 
 # Set the parameter overrides (if any)
 PARAMETERS='ParameterKey=AWSRegion,ParameterValue='$AWS_REGION
 
 # Create or update the StackSet
 if aws cloudformation describe-stack-set --stack-set-name $STACKSET_NAME --region $AWS_REGION >/dev/null 2>&1; then
-    # Stack set exists, update it
-    aws cloudformation update-stack-set \
-      --stack-set-name $STACKSET_NAME \
-      --use-previous-template \
-      --parameters $PARAMETERS \
-      --region $AWS_REGION
-  else
-    # Stack set doesn't exist, create it
-    aws cloudformation create-stack-set \
-      --stack-set-name $STACKSET_NAME \
-      --template-body file://$TEMPLATE_FILE \
-      --parameters $PARAMETERS \
-      --permission-model SELF_MANAGED \
-      --capabilities CAPABILITY_IAM \
-      --region $AWS_REGION
-  fi
+  # Stack set exists, update it
+  aws cloudformation update-stack-set \
+    --stack-set-name $STACKSET_NAME \
+    --use-previous-template \
+    --parameters $PARAMETERS \
+    --region $AWS_REGION
+else
+  # Stack set doesn't exist, create it
+  aws cloudformation create-stack-set \
+    --stack-set-name $STACKSET_NAME \
+    --template-body file://$TEMPLATE_FILE \
+    --parameters $PARAMETERS \
+    --permission-model SELF_MANAGED \
+    --capabilities CAPABILITY_IAM \
+    --region $AWS_REGION
+fi
 
 # Create stack instances in the target accounts and regions
 OPERATION_ID=$(aws cloudformation create-stack-instances \
